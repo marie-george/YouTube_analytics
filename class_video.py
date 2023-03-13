0a24a5297f3db506aa44b2793f7b70823967433d
marie-george <maria85night@gmail.com>
2023-03-13 22:05:31 +0300

    added try/exception block to class_video

--- class_video.py
+++ class_video.py
@@ -5,14 +5,32 @@ from class_mixin_service import MixinService
 class Video(MixinService):
     def __init__(self, video_id):
         self.video_id = video_id
-        youtube = self.get_service()
-        self.video_response = youtube.videos().list(part='snippet,statistics', id=video_id).execute()
-        self.title = self.video_response['items'][0]['snippet']['title']
-        self.view_count = self.video_response['items'][0]['statistics']['viewCount']
-        self.like_count = self.video_response['items'][0]['statistics']['likeCount']
-
+        try:
+            youtube = self.get_service()
+            self.video_response = youtube.videos().list(part='snippet,statistics', id=video_id).execute()
+            self.title = self.video_response['items'][0]['snippet']['title']
+            self.view_count = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+        except Exception:
+            self.video_response = None
+            self.title = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
-        return self.title
+        try:
+            if self.title is not None:
+                return self.title
+            else:
+                raise TypeError
+        except TypeError:
+            return f'Невозможно получить данные по этому ID'
+
+
+video1 = Video('broken_video_id')
+
+print(video1)
+print(video1.title)
+print(video1.like_count)
 
 
